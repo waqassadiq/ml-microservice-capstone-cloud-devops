0363aaa2b39fc4395fb5a31c3214b948142a3847
Waqas <wakassadik@gmail.com>
2020-08-06 18:53:23 +0500

    removing sudo from run_kub

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -11,4 +11,4 @@ whoami
 aws sts get-caller-identity
 # Step 3: 
 # run the following command
-sudo eksctl create cluster --name eks-capestone --version 1.17 --region us-west-2 --nodegroup-name linux-nodes --node-type t2.micro --nodes 2 --nodes-min 1 --nodes-max 2 --ssh-access --ssh-public-key jenkins-key-pair.pub --managed
+eksctl create cluster --name eks-capestone --version 1.17 --region us-west-2 --nodegroup-name linux-nodes --node-type t2.micro --nodes 2 --nodes-min 1 --nodes-max 2 --ssh-access --ssh-public-key jenkins-key-pair.pub --managed
